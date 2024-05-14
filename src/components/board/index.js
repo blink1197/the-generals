c86d84154bd352e30d58a98adaef923703f35cf0
blink1197 <cgbagotsay11@gmail.com>
2024-05-14 21:57:15 +0800

    addead feature click-move a piece

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -9,6 +9,23 @@ const NUM_COLS = 9;
 function Board() {
     
     const [boardState, setBoardState] = useState(INITIAL_BOARD_STATE);
+    const [selectedCell, setSelectedCell] = useState("");
+
+    const clickMovePiece = (event) => {
+        const { id } = event.currentTarget;
+        if (selectedCell !== "") {
+            const selectedPiece = boardState[selectedCell];
+            if (!boardState[id] && selectedPiece) {
+                const updatedBoardState = { ...boardState };
+                updatedBoardState[id] = selectedPiece;
+                updatedBoardState[selectedCell] = null;
+                setBoardState(updatedBoardState);
+            }
+            setSelectedCell("");
+        } else {
+            setSelectedCell(id);
+        }
+    };
 
     const generateCellId = (row, col) => {
         const columns = 'ABCDEFGHI';
@@ -18,14 +35,20 @@ function Board() {
     const cells = Array.from({ length: NUM_ROWS * NUM_COLS }, (_, index) => {
         const row = Math.floor(index / NUM_COLS);
         const col = index % NUM_COLS;
-        return generateCellId(NUM_ROWS - row - 1, col); // Adjust row index to start from the bottom
+        return generateCellId(NUM_ROWS - row - 1, col); 
     });
 
     return (
         <div className="flex items-center justify-center min-h-screen mx-auto my-auto bg-white">
             <div className="grid grid-cols-9 gap-1">
                 {cells.map((cell) => (
-                    <Cell key={cell} cellId={cell} pieceId={boardState[cell]}/>
+                    <Cell 
+                        key={cell} 
+                        cellId={cell} 
+                        pieceId={boardState[cell]} 
+                        clickMovePiece={clickMovePiece}
+                        isSelected={cell === selectedCell ? true : false}
+                    />
                 ))}
             </div>
         </div>
