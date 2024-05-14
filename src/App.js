dad22eb3f96860cbba1020b3f9a3ec9d078ed39d
CrisB. <100068688+blink1197@users.noreply.github.com>
2024-05-15 01:42:39 +0800

    removed dnkit import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {DndContext} from '@dnd-kit/core';
+
 import Board from './components/board';
 
 function App() {
   return (
-    <DndContext>
+
       <Board />
-    </DndContext>
+    
   );
 }
 
