0cd06fbe7536c10c83838c04307896ed50fa8a4a
blink1197 <cgbagotsay11@gmail.com>
2024-05-16 16:27:33 +0800

    added socket.io module

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import Board from './components/board';
 import PlayerCard from './components/playerCard';
 import MoveHistory from './components/moveHistory';
 import SideBar from './components/sidebar';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3001")
+
 
 function App() {
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnected from server');
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+    };
+  }, []);
+  
   return (
     <div className='flex h-full font-outfit'>
       <div className='flex items-center mx-auto'>
