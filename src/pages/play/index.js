ffa1a1f2a62b7717ed85169049974807e2c83999
blink1197 <cgbagotsay11@gmail.com>
2024-05-17 00:26:11 +0800

    added routing to pages

--- src/pages/play/index.js
+++ src/pages/play/index.js
@@ -1,9 +1,18 @@
+import Board from '../../components/board';
+import PlayerCard from '../../components/playerCard';
+import MoveHistory from '../../components/moveHistory';
+
 function Play () {
     
     return (
-        <div>
-            Play
-        </div>
+        <div className='flex items-center mx-auto'>
+            <div className='flex flex-col ml-10 w-fit'>
+                <PlayerCard color={'B'}/>
+                <Board />
+                <PlayerCard />
+            </div>
+            <MoveHistory />
+      </div>
     );
 
 }
