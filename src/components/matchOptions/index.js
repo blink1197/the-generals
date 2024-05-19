6cb073ff24577c339c3f433717e44aebdc54fc5d
blink1197 <cgbagotsay11@gmail.com>
2024-05-19 23:50:57 +0800

    added card description

--- src/components/matchOptions/index.js
+++ src/components/matchOptions/index.js
@@ -3,10 +3,18 @@ function MatchOptions(props) {
     return (
         <div className="h-full">
             <div className="grid w-full h-full grid-cols-1 gap-4 p-10 md:grid-cols-2">
-                <div className="border border-blue-500"></div>
-                <div className="border border-blue-500"></div>
-                <div className="border border-blue-500"></div>
-                <div className="border border-blue-500"></div>
+                <div className="border border-blue-500">
+                    Classic
+                </div>
+                <div className="border border-blue-500">
+                    AI Match
+                </div>
+                <div className="border border-blue-500">
+                    Friendly Match
+                </div>
+                <div className="border border-blue-500">
+                    Ranked
+                </div>
             </div>
         </div>
     );
