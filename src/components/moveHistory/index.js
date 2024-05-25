a28ec999abb22e64f7b644939d280620dc642d42
blink1197 <cgbagotsay11@gmail.com>
2024-05-25 11:45:45 +0800

    adjusted move-history

--- src/components/moveHistory/index.js
+++ src/components/moveHistory/index.js
@@ -1,7 +1,7 @@
 const MoveHistory = ({ playerMoves, matchStatus }) => {
     return (
-        <div className="w-full mt-4 rounded-md xl:w-60">
-            <table className="w-full text-[12px] text-center border">
+        <div className="w-full mt-4 border border-gray-200 rounded-md shadow-sm xl:w-60">
+            <table className="w-full text-[12px] text-center ">
                 <thead className="w-full">
                     <tr className="w-full bg-gray-100">
                         <td className="w-1/4">
@@ -18,19 +18,24 @@ const MoveHistory = ({ playerMoves, matchStatus }) => {
                         </td>
                     </tr>
                 </thead>
-                <tbody className="w-full">
-                    {(matchStatus === 'gameProper' && playerMoves.length > 1) &&
-                        playerMoves.slice(1).reverse().map((move, index) => (
-                            <tr key={index}>
-                                <td>{move.turnNumber}</td>
-                                <td>{move.pieceId}</td>
-                                <td>{move.from}</td>
-                                <td>{move.to}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
             </table>
+            <div className="w-full overflow-y-scroll max-h-32 ">
+                <table className="w-full text-[12px] text-center table-auto">
+                    <tbody className="w-full overflow-y-scroll ">
+                        {(matchStatus === 'gameProper' && playerMoves.length > 1) &&
+                            playerMoves.slice(1).reverse().map((move, index) => (
+                                <tr key={index} className={`${index % 2 !== 0 ? 'bg-gray-100' : 'bg-white'}`}>
+                                    <td className="w-1/4">{move.turnNumber}</td>
+                                    <td className="w-1/4">{move.pieceId}</td>
+                                    <td className="w-1/4">{move.from}</td>
+                                    <td className="w-1/4">{move.to}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
